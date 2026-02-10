b3bacf64a45a34232dde6b73a0d4a0425a97b4de
vijayguhan10 <vijayguhan10@gmail.com>
2026-02-10 21:28:30 +0530

    Added the project dependencies

---
+++ vuega-backend/src/main/java/net/vuega/vuega_backend/VuegaBackendApplication.java
@@ -0,0 +1,13 @@
+package net.vuega.vuega_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuegaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuegaBackendApplication.class, args);
+	}
+
+}
---
+++ vuega-backend/src/test/java/net/vuega/vuega_backend/VuegaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vuega.vuega_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuegaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
