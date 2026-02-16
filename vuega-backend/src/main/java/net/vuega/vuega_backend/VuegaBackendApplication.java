e9662e8b59f1ffc3f4d20f4c98ba05a730511859
vijayguhan10 <vijayguhan10@gmail.com>
2026-02-16 21:05:38 +0530

    Configured the Operator Config Data Models

--- vuega-backend/src/main/java/net/vuega/vuega_backend/VuegaBackendApplication.java
+++ vuega-backend/src/main/java/net/vuega/vuega_backend/VuegaBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VuegaBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Vuega Backend Application...");
 		SpringApplication.run(VuegaBackendApplication.class, args);
 	}
 
