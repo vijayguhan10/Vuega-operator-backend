3d04d033f9934f12b5487aad8e6548ef07fa35ae
vijayguhan10 <vijayguhan10@gmail.com>
2026-02-16 21:40:29 +0530

    Configured Auto startup"

--- vuega-backend/src/main/java/net/vuega/vuega_backend/Model/operator_config/OperatorConfig.java
+++ vuega-backend/src/main/java/net/vuega/vuega_backend/Model/operator_config/OperatorConfig.java
@@ -1,8 +1,17 @@
 package net.vuega.vuega_backend.Model.operator_config;
 
-import jakarta.persistence.*;
-import lombok.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "operator_config")
@@ -11,7 +20,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class OperatorConfig {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "operator_id")
