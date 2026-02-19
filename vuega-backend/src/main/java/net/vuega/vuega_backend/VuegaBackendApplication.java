f63372a2690f2d20f2368c3afe746eed372e489e
vijayguhan10 <vijayguhan10@gmail.com>
2026-02-19 22:46:17 +0530

    Added the Validation Package

--- vuega-backend/src/main/java/net/vuega/vuega_backend/VuegaBackendApplication.java
+++ vuega-backend/src/main/java/net/vuega/vuega_backend/VuegaBackendApplication.java
@@ -2,8 +2,10 @@ package net.vuega.vuega_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling   // Required for @Scheduled on SeatService.releaseExpiredLocks()
 public class VuegaBackendApplication {
 
 	public static void main(String[] args) {
